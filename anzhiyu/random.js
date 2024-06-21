9857080bc925283df5334c4fc652440d2c96a769
waljj123 <1460567153@qq.com>
2024-06-21 17:39:41 +0800

    Site updated: 2024-06-21 17:39:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/21/hello-world/","2024/06/20/threadpool/"];function toRandomPost(){
+var posts=["2024/06/20/threadpool/","2024/06/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
