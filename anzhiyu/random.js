2bd0c3e37ab3185a29dda9d1d70c10480c1f9a27
waljj123 <1460567153@qq.com>
2024-06-22 14:53:38 +0800

    Site updated: 2024-06-22 14:53:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/20/threadpool/","2024/06/21/hello-world/"];function toRandomPost(){
+var posts=["2024/06/20/threadpool/","2024/06/22/mysql-engine/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
